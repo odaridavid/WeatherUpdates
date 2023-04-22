7aa65c01a2ab885df416647635b9478c8d28a2b5
odaridavid <davidkibzodari@gmail.com>
2023-04-22 13:31:29 +0200

    remove sizing extension

--- app/src/main/java/dev/davidodari/weatherupdates/ui/theme/Sizing.kt
+++ app/src/main/java/dev/davidodari/weatherupdates/ui/theme/Sizing.kt
@@ -1,8 +1,8 @@
 package dev.davidodari.weatherupdates.ui.theme
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.unit.dp
 
+//TODO Make part of material theme through extension
 object Sizing {
     val large = 32.dp
     val medium = 16.dp
@@ -11,5 +11,3 @@ object Sizing {
     val none = 0.dp
 }
 
-val MaterialTheme.sizing
-    get() = Sizing
