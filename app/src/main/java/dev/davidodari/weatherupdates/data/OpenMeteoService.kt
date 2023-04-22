3f3ad41c0de005e44134ef34be8dc63c41111707
odaridavid <davidkibzodari@gmail.com>
2023-04-22 13:34:19 +0200

    add missing hourly query

--- app/src/main/java/dev/davidodari/weatherupdates/data/OpenMeteoService.kt
+++ app/src/main/java/dev/davidodari/weatherupdates/data/OpenMeteoService.kt
@@ -11,8 +11,10 @@ interface OpenMeteoService {
     suspend fun getWeatherData(
         @Query("latitude") latitude: Double,
         @Query("longitude") longitude: Double,
+        @Query("hourly") hourly:String = "temperature_2m",
         @Query("temperature_unit") temperatureUnit: String = Units.METRIC.value,
-        @Query("forecast_days") forecastDays: Int = 7
+        @Query("forecast_days") forecastDays: Int = 1,
+        @Query("current_weather") includeCurrentWeather: Boolean = true
     ): Response<WeatherResponse>
 
 }
