125e9aa31d932ef46eb21886c78d174b7ab5e8de
odaridavid <davidkibzodari@gmail.com>
2023-04-22 13:30:58 +0200

    update min sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "dev.davidodari.weatherupdates"
-        minSdk = 23
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
